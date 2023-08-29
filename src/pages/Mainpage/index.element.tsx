4672108331b9765eeea9da176aa9c66f25fde89c
moonki <moonki0623@naver.com>
2023-08-29 16:07:01 +0900

    feat: mainpage style setting

---
+++ src/pages/Mainpage/index.element.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const MainpageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.palette.white};
+`;
---
+++ src/pages/Mainpage/index.tsx
@@ -0,0 +1,10 @@
+import { MainpageContainer } from "./index.element";
+
+const Mainpage = () => {
+  return (
+    <MainpageContainer>
+      <>MainPage</>
+    </MainpageContainer>
+  );
+};
+export default Mainpage;

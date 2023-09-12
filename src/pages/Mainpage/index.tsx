43bfc6fd3730900ef669abcefaea1f4456a8c4f5
moonki <moonki0623@naver.com>
2023-09-13 03:18:16 +0900

    feat: Main Chat Page 구현

--- src/pages/Mainpage/index.tsx
+++ src/pages/Mainpage/index.tsx
@@ -1,9 +1,105 @@
-import { MainpageContainer } from "./index.element";
+import { useEffect, useRef, useState } from "react";
+import moment from "moment";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { useParams } from "react-router-dom";
+
+import ChatBubble from "@/components/ChatBubble";
+import { IChatData } from "@/interfaces";
+import { chatToggleState, talkState } from "@/states";
+import DummyChatData from "@/assets/datas/chatData.json";
+import alienSrc from "@/assets/images/alien.png";
+import { ChatLineWrapper, ChatTime, MainpageContainer } from "./index.element";
 
 const Mainpage = () => {
+  const [chatData, setChatData] = useState(DummyChatData);
+  const talker = useRecoilValue(talkState);
+  const mainpageRef = useRef<HTMLDivElement>(null);
+  const [chatRefresh, setChatRefresh] = useRecoilState(chatToggleState);
+  const { chatId } = useParams();
+
+  const setLocalStorage = () => {
+    if (localStorage.getItem(`chatData_${chatId}`) === null) {
+      localStorage.setItem(
+        `chatData_${chatId}`,
+        JSON.stringify(
+          chatData.find((el) => el.id === Number(chatId))?.chatData
+        )
+      );
+    }
+  };
+
+  useEffect(() => {
+    // 이유는 모르겠는데, setTimeout에 넣어야 scrollHeight가 제대로 작동함
+    const scrollTimeout = setTimeout(() => {
+      mainpageRef.current?.scrollTo(0, mainpageRef.current.scrollHeight);
+    }, 0);
+    return () => clearTimeout(scrollTimeout);
+  }, [chatData]);
+
+  useEffect(() => {
+    if (chatId !== undefined) {
+      if (localStorage.getItem(`chatData_${chatId}`) === null) {
+        setLocalStorage();
+      } else {
+        setChatData(
+          chatData.map((el) => {
+            if (el.id === Number(chatId)) {
+              return {
+                ...el,
+                chatData: JSON.parse(
+                  localStorage.getItem(`chatData_${chatId}`)!
+                ),
+              };
+            } else {
+              return el;
+            }
+          })
+        );
+      }
+      setChatRefresh(false);
+    }
+  }, [chatRefresh]);
+
   return (
-    <MainpageContainer>
-      <>MainPage</>
+    <MainpageContainer ref={mainpageRef}>
+      {chatData &&
+        chatData
+          .find((el) => el.id === Number(chatId))!
+          .chatData.map((chat) => {
+            // Type Assertion
+            const assertedChat = chat as IChatData;
+            return (
+              <div key={assertedChat.createdAt}>
+                <ChatTime>
+                  {moment(chat.createdAt).format("MMM DD, YYYY, h:mm A")}
+                </ChatTime>
+                <ChatLineWrapper from={assertedChat.from === talker ? 1 : 2}>
+                  {assertedChat.from === 2 && talker === 1 && (
+                    <span>
+                      <img
+                        src={require(`../../assets/images/${chatData[0].partner.imgSrc}`)}
+                        alt="partner profile"
+                      />
+                    </span>
+                  )}
+                  {assertedChat.from === 1 && talker === 2 && (
+                    <span>
+                      <img
+                        src={alienSrc}
+                        alt="my profile"
+                      />
+                    </span>
+                  )}
+                  <ChatBubble
+                    from={assertedChat.from === talker ? 1 : 2}
+                    text={assertedChat.content}
+                    time={assertedChat.createdAt}
+                    liked={assertedChat.like}
+                  />
+                </ChatLineWrapper>
+              </div>
+            );
+          })}
     </MainpageContainer>
   );
 };

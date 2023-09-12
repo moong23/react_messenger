101ff6330321d5f196301634b50ecfae9342fdb1
moonki <moonki0623@naver.com>
2023-09-13 03:16:35 +0900

    style: chat bubble style

---
+++ src/components/ChatBubble/index.element.tsx
@@ -0,0 +1,44 @@
+import styled, { css } from "styled-components";
+
+interface ICBCProps {
+  from: "me" | "partner";
+  liked: boolean;
+}
+export const ChatBubbleContainer = styled.div<ICBCProps>`
+  max-width: 263px;
+  width: fit-content;
+  white-space: pre-wrap;
+  padding: 1rem 1.4rem;
+  border-radius: 2rem;
+  position: relative;
+
+  background-color: ${(props) =>
+    props.from === "me"
+      ? props.theme.palette.blue2
+      : props.theme.palette.gray2};
+
+  color: ${(props) =>
+    props.from === "me"
+      ? props.theme.palette.white1
+      : props.theme.palette.black};
+
+  ${(props) => props.theme.typography.Body};
+
+  ${(props) =>
+    props.liked &&
+    css`
+      ::after {
+        content: "❤️";
+        position: absolute;
+        bottom: 0;
+        right: 0rem;
+        transform: translate(0, 50%);
+        font-size: 1.5rem;
+        border: 2px solid ${(props) => props.theme.palette.white1};
+        background-color: ${(props) => props.theme.palette.gray2};
+        z-index: 100;
+        padding: 0.2rem 0.5rem;
+        border-radius: 5rem;
+      }
+    `}
+`;

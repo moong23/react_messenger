ace69b3ee5163a0597c847bc9284f52c3b7f85a4
moonki <moonki0623@naver.com>
2023-09-13 03:15:46 +0900

    feat: bottombar 라우팅 변경

--- src/components/Bottombar/index.tsx
+++ src/components/Bottombar/index.tsx
@@ -1,9 +1,12 @@
 import { BottomBarContainer } from "./index.element";
 import bottomBarFloater from "@/assets/images/bottomBar.png";
-
+import { useLocation } from "react-router-dom";
+import ChatBottomFloater from "../ChatBottomFloater";
 const BottomBar = () => {
+  const { pathname } = useLocation();
   return (
-    <BottomBarContainer>
+    <BottomBarContainer extend={pathname.includes("/chat") ? 1 : 0}>
+      {pathname.includes("/chat/") && <ChatBottomFloater />}
       <img
         src={bottomBarFloater}
         alt="bottomBar"
